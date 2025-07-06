4bb6b7b0960cf97408d38f994036fc650b957833
Anas <anas@Anass-MacBook-Air.local>
2025-07-06 11:44:14 +0530

    bridge testing

--- script/DeployMockUSDT.s.sol
+++ script/DeployMockUSDT.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import "forge-std/Script.sol";
-import "../src/test/mocks/MockUSDT.sol";
+import "../src/mocks/MockUSDT.sol";
 
 contract DeployMockUSDTScript is Script {
     function run() external {
